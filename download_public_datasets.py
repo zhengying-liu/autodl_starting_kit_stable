5f53dc5599c4c08d56ac15adb6f3ba7ca9d9211d
Adrien Pavao <adrien.pavao@gmail.com>
2019-07-16 17:17:55 +0200

    Update download_public_datasets.py after re-uploading datasets on Codalab.

--- download_public_datasets.py
+++ download_public_datasets.py
@@ -13,24 +13,24 @@ def main(*argv):
   dataset_names = ['Munster', 'Chucky','Pedro', 'Decal',
                    'Hammer', 'Kreatur', 'Katze', 'Kraut']
   data_urls ={
-      'Munster':'https://autodl.lri.fr/my/datasets/download/6662aa6e-75ab-439c-bf98-97dd11401053',
-      'Chucky':'https://autodl.lri.fr/my/datasets/download/d06aa5fc-1fb5-4283-8e05-abed4ccdd975',
-      'Pedro':'https://autodl.lri.fr/my/datasets/download/61a074cd-e909-4d49-b313-7da0d4f7dc8b',
-      'Decal':'https://autodl.lri.fr/my/datasets/download/dfd93c39-e0d4-41b2-b332-4dd002676e05',
-      'Hammer':'https://autodl.lri.fr/my/datasets/download/eb569948-72f0-4002-8e4d-479a27766cbf',
-      'Kreatur':'https://autodl.lri.fr/my/datasets/download/9dd0fded-7c58-4768-805d-875e82191043',
-      'Katze':'https://autodl.lri.fr/my/datasets/download/fc318649-5224-458c-bb17-0d89b76a75dd',
-      'Kraut':'https://autodl.lri.fr/my/datasets/download/39614747-95bd-49fb-aded-2437dd1675ba'
+      'Munster':'https://autodl.lri.fr/my/datasets/download/d29000a6-b5b8-4ccf-9050-2686af874a71',
+      'Chucky':'https://autodl.lri.fr/my/datasets/download/cf2176c2-5454-4d07-9c4e-758e3c5bcb31',
+      'Pedro':'https://autodl.lri.fr/my/datasets/download/d556ca67-01c7-4a8d-9a74-a2bd9c06414d',
+      'Decal':'https://autodl.lri.fr/my/datasets/download/31a34c03-a75c-4e0f-b72d-3723ba303dac',
+      'Hammer':'https://autodl.lri.fr/my/datasets/download/3507841e-59fe-4598-a27e-a9e170b26e44',
+      'Kraut':'https://autodl.lri.fr/my/datasets/download/a1d9f706-cf8d-4a63-a544-552d6b85d6c4',
+      'Katze':'https://autodl.lri.fr/my/datasets/download/611a42fa-da42-4a30-8c7a-69230d9eeb92',
+      'Kreatur':'https://autodl.lri.fr/my/datasets/download/c240df57-b144-41df-a059-05bc859d2621'      
   }
   solution_urls = {
-      'Munster':'https://autodl.lri.fr/my/datasets/download/f3a61a40-b1f1-4ded-bc55-fb730a12f4c4',
-      'Chucky':'https://autodl.lri.fr/my/datasets/download/29932707-21cc-4670-a7db-cdc246a8ab71',
-      'Pedro':'https://autodl.lri.fr/my/datasets/download/852c1e68-5e91-477e-bef0-824b503814e8',
-      'Decal':'https://autodl.lri.fr/my/datasets/download/d72cba79-3051-4779-b624-e50335aad874',
-      'Hammer':'https://autodl.lri.fr/my/datasets/download/c3729c98-4755-47a2-b764-a4159c5ca152',
-      'Kreatur':'https://autodl.lri.fr/my/datasets/download/d79782d6-f967-4883-b417-1206566dd69c',
-      'Katze':'https://autodl.lri.fr/my/datasets/download/a0713a0d-1214-427c-b67e-302d4e3efe0c',
-      'Kraut':'https://autodl.lri.fr/my/datasets/download/0e1e5166-40a5-4daa-a053-8bb506fac5a1'
+      'Munster':'https://autodl.lri.fr/my/datasets/download/5a24d8f3-dfb6-4935-b798-14baccda695f',
+      'Chucky':'https://autodl.lri.fr/my/datasets/download/ba4837bf-275d-43a6-a481-d03dce7ba127',
+      'Pedro':'https://autodl.lri.fr/my/datasets/download/9993ea27-955e-4faa-9d28-4a7dfe1fcc55',
+      'Decal':'https://autodl.lri.fr/my/datasets/download/cc93c74c-2732-4e7d-ae7f-a2c3bc555360',
+      'Hammer':'https://autodl.lri.fr/my/datasets/download/e5b6188f-a377-4a5d-bbe1-a586716af487',
+      'Kraut':'https://autodl.lri.fr/my/datasets/download/47ff016d-cc66-47a9-945d-bc01fd9096c9',
+      'Katze':'https://autodl.lri.fr/my/datasets/download/a04de92e-b04b-49a6-96c2-5910c64f9b3c',
+      'Kreatur':'https://autodl.lri.fr/my/datasets/download/31ecdb19-c25a-420f-9764-8d1783705deb'
   }
 
   def _HERE(*args):
